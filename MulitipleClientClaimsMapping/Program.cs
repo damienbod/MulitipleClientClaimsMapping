6c4eb50c8ac9b204147c9f603142a1dccb0fca8c
damienbod <damien_bod@hotmail.com>
2023-11-07 11:27:59 +0100

    claims mapping

--- MulitipleClientClaimsMapping/Program.cs
+++ MulitipleClientClaimsMapping/Program.cs
@@ -1,9 +1,47 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var authConfigurations = builder.Configuration.GetSection("AuthConfigurations");
+var stsServer = authConfigurations["StsServer"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect(options =>
+    {
+        options.SignInScheme = "Cookies";
+        options.Authority = stsServer;
+        options.RequireHttpsMetadata = true;
+        options.ClientId = "codeflowpkceclient";
+        options.ClientSecret = "codeflow_pkce_client_secret";
+        options.ResponseType = "code";
+        options.UsePkce = true;
+        options.Scope.Add("profile");
+        options.Scope.Add("offline_access");
+        options.SaveTokens = true;
+        options.MapInboundClaims = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "name"
+        };
+    });
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
 var app = builder.Build();
+
+IdentityModelEventSource.ShowPII = true;
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,6 +56,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
