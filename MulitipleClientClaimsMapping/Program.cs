79080469507cb1e971b524cdb4901089a182d584
damienbod <damien_bod@hotmail.com>
2023-11-12 16:43:25 +0100

    middleware

--- MulitipleClientClaimsMapping/Program.cs
+++ MulitipleClientClaimsMapping/Program.cs
@@ -46,11 +46,9 @@ var app = builder.Build();
 IdentityModelEventSource.ShowPII = true;
 //JsonWebTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
